2a3e928f26bdfc458d4379a8e40f84da8a69de92
Gkiokan Sali <gkiokan@hotmail.de>
2022-05-03 16:55:38 +0200

    initial setup

---
+++ src/app.js
@@ -0,0 +1,9 @@
+console.log("START CLI Server Application")
+
+import helper from './helper'
+//
+let interfaces = helper.getInterfaces()
+
+console.log(interfaces)
+
+console.log("END")
---
+++ src/helper.js
@@ -0,0 +1,30 @@
+import os from 'os'
+
+export default {
+
+    getInterfaces(){
+        let ifaces = [];
+        Object.keys(os.networkInterfaces()).forEach(function (ifname) {
+          var alias = 0;
+          os.networkInterfaces()[ifname].forEach(function (iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+              return;
+            }
+
+            if (alias >= 1) {
+              ifaces.push({
+                title: `${ifname}-${alias}:${iface.address}`,
+                ip: iface.address
+              });
+            } else {
+              ifaces.push({
+                title: `${ifname}: ${iface.address}`,
+                ip: iface.address
+              });
+            }
+            ++alias;
+          });
+        });
+        return ifaces
+    },
+}
